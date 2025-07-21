6c7627ea32769b181abead026d3f61cf22403706
FAZuH <fazuhhh@proton.me>
2025-07-22 06:47:26 +0700

    fazuh: remove transcluded content title

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -137,8 +137,10 @@ function renderTranscludes(
             return
           }
 
+          const isEmbed = classNames.includes("embed")
+          const sliceStart = isEmbed ? startIdx + 1 : startIdx
           node.children = [
-            ...(page.htmlAst.children.slice(startIdx, endIdx) as ElementContent[]).map((child) =>
+            ...(page.htmlAst.children.slice(sliceStart, endIdx) as ElementContent[]).map((child) =>
               normalizeHastElement(child as Element, slug, transcludeTarget),
             ),
             {
@@ -152,22 +154,27 @@ function renderTranscludes(
           ]
         } else if (page.htmlAst) {
           // page transclude
+          const isEmbed = classNames.includes("embed")
           node.children = [
-            {
-              type: "element",
-              tagName: "h1",
-              properties: {},
-              children: [
-                {
-                  type: "text",
-                  value:
-                    page.frontmatter?.title ??
-                    i18n(cfg.locale).components.transcludes.transcludeOf({
-                      targetSlug: page.slug!,
-                    }),
-                },
-              ],
-            },
+            ...(isEmbed
+              ? []
+              : [
+                  {
+                    type: "element",
+                    tagName: "h1",
+                    properties: {},
+                    children: [
+                      {
+                        type: "text",
+                        value:
+                          page.frontmatter?.title ??
+                          i18n(cfg.locale).components.transcludes.transcludeOf({
+                            targetSlug: page.slug!,
+                          }),
+                      },
+                    ],
+                  },
+                ]),
             ...(page.htmlAst.children as ElementContent[]).map((child) =>
               normalizeHastElement(child as Element, slug, transcludeTarget),
             ),
--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -268,7 +268,7 @@ export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>>
                     return {
                       type: "html",
                       data: { hProperties: { transclude: true } },
-                      value: `<blockquote class="transclude" data-url="${url}" data-block="${block}" data-embed-alias="${alias}"><a href="${
+                      value: `<blockquote class="transclude ${value.startsWith("!") ? "embed" : ""}" data-url="${url}" data-block="${block}" data-embed-alias="${alias}"><a href="${
                         url + anchor
                       }" class="transclude-inner">Transclude of ${url}${block}</a></blockquote>`,
                     }
