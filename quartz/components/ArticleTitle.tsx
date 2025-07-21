7f0bb77d044e91f87bc7e58ad822ccf253fa17aa
FAZuH <fazuhhh@proton.me>
2025-07-22 06:47:01 +0700

    fazuh: use aliases property as article title

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,8 +3,23 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
-  if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+  const aliases = fileData.frontmatter?.aliases
+  const alias = fileData.frontmatter?.alias
+
+  let displayTitle = title
+
+  if (aliases && Array.isArray(aliases) && aliases.length > 0) {
+    displayTitle = aliases[0]
+  } else if (alias && typeof alias === 'string') {
+    displayTitle = alias
+  }
+
+  if (displayTitle && displayTitle.endsWith('.md')) {
+    displayTitle = displayTitle.slice(0, -3)
+  }
+
+  if (displayTitle) {
+    return <h1 class={classNames(displayClass, "article-title")}>{displayTitle}</h1>
   } else {
     return null
   }
